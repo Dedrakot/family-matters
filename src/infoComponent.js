24b30d664c730d34e985326f08f03ac1bdea4b7f
Ivan Gladyshev <ivan.gladyshev@actimind.com>
2023-08-08 23:19:31 +0700

    wiki in subpath, open wiki page on the same tab

--- src/infoComponent.js
+++ src/infoComponent.js
@@ -1,14 +1,15 @@
 export default function infoComponent(wikiUrl) {
     return {
         button: (info, card_dim) => {
-            return info !== undefined ? ` 
+            return ` 
             <image class="info" style="cursor: pointer" href="img/info.svg"
                 x="${card_dim.w-28}px" y="${card_dim.h-28}px">
                 <title>Ссылка на wiki</title>
-            </image>` : ''
+            </image>`
         },
         listener: (store, {card, d}) => {
-            window.open(wikiUrl + '/' + d.data.data.info, '_blank');
+            const item = d.data
+            window.open(wikiUrl + '/' + (item.data.info || ("люди/" + item.id)), "_self");
         },
         querySelector: '.info'
     }
